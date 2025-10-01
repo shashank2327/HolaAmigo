acfcc5ce7b331d24cff11ca6aeae860033eabaa7
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-10-01 23:09:36 +0530

    message routes and controllers setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js"
 import dotenv from "dotenv";
 import {connectDB} from "./lib/db.js"
 import cookieParser from "cookie-parser"
+
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 
 dotenv.config();
 const app = express();
@@ -10,7 +12,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json()); // allows you to extract the json data out of body.
 app.use(cookieParser());
+
+
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on port:" + PORT);
