bda2aecde6e0e18d20f914179932234dcb082588
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-10-01 19:12:27 +0530

    authentication setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json()) // allows you to extract the json data out of body.
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
