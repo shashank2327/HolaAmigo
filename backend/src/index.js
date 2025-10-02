d597a85b98d29f21556ff311aeb281842fc25729
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-10-03 00:40:28 +0530

    signup page

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import {connectDB} from "./lib/db.js"
 import cookieParser from "cookie-parser"
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
@@ -12,6 +13,11 @@ const PORT = process.env.PORT;
 
 app.use(express.json()); // allows you to extract the json data out of body.
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+  })
+);
 
 
 app.use("/api/auth", authRoutes);
