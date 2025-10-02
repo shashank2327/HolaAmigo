beff6096195578edce46341292a666f99bbdc726
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-10-02 22:08:06 +0530

    tailwind setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,8 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-
-
-import Navbar from './components/Navbar'
-
-import HomePage from './pages/HomePage'
-import SignUpPage from './pages/SignUpPage'
-import LoginPage from './pages/LoginPage'
-import SettingsPage from './pages/SettingsPage'
-import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
-    <div>
-
-      <Navbar />
-
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/settings' element={<SettingsPage />} />
-        <Route path='/profile' element={<ProfilePage />} />
-
-      </Routes>
-    </div>
+    <div>App</div>
   )
 }
 
