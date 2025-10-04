e4d7a4574f09bb1e05ef549b78f24b3cd3270bc8
Shashank Kumar Roy <152942685+shashank2327@users.noreply.github.com>
2025-10-04 22:28:40 +0530

    HomePage - ChatContainer

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,9 +1,10 @@
 import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
+import { AwardIcon } from "lucide-react";
 
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
     messages: [],
     users: [],
     selectedUser: null,
@@ -31,6 +32,16 @@ export const useChatStore = create((set) => ({
             toast.error(error.response.data.message);
         } finally {
             set({ isMessagesLoading: false });
+        }
+    },
+
+    sendMessage: async (messageData) => {
+        const { selectedUser, messages } = get();
+        try {
+            const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+            set({ messages: [...messages, res.data] });
+        } catch (error) {
+            toast.error(error.response.data.message);
         }
     },
 
